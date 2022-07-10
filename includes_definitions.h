9a6accb8aec7090c3704f6753abe9972c7d670eb
Dorin Ingrum <dorin_ingrum@outlook.com>
2022-07-10 11:28:16 +0200

    Working solution that supports both RX and TX.
    After establishing connection MCU sends "Hello world....Goodbye"
    THEN:
    	PC -> "1" -> MCU lights up a diode
    	PC -> "0" -> MCU turns off said diode

--- includes_definitions.h
+++ includes_definitions.h
@@ -7,10 +7,13 @@
 #define INCLUDES_AND_DEFINITIONS_H
 #include <avr/interrupt.h>
 #include <avr/io.h>
+#include <string.h>
+#define F_CPU 16000000 // CPU frequency
+#include <util/delay.h>
 #define cbi(sfr, bit) (_SFR_BYTE(sfr) &= ~_BV(bit)) // clears i-th bit
 #define sbi(sfr, bit) (_SFR_BYTE(sfr) |= _BV(bit))  // sets i-th bit
-#define F_CPU 16000000                              // CPU frequency
 #define BAUD 9600                                   // baudrate
 #define BRC ((F_CPU / 16 / BAUD) - 1)               // ??
 #define RX_BUFFER_SIZE 128                          // receive buffer
+#define TX_BUFFER_SIZE 128
 #endif
--- main.c
+++ main.c
@@ -9,14 +9,18 @@
 
 #include "includes_definitions.h"
 #include "rx.h"
-
+#include "tx.h"
 int main(void) {
   UBRR0H = (BRC >> 8);
   UBRR0L = BRC;
-  UCSR0B = (1 << RXEN0) | (1 << RXCIE0);
+  UCSR0B = (1 << TXEN0) | (1 << TXCIE0) | (1 << RXEN0) | (1 << RXCIE0);
   UCSR0C = (1 << UCSZ01) | (1 << UCSZ00);
   DDRB = (1 << PORTB1);
   sei();
+  serialWrite("Hello\n\r");
+  serialWrite("World\n\r");
+  _delay_ms(1500);
+  serialWrite("Goodbye\n\r");
   while (1) {
     char c = getChr();
     if (c == '1') {
@@ -30,5 +34,13 @@ ISR(USART_RX_vect) {
   rxBuffer[rxWritePos++] = UDR0;
   if (rxWritePos >= RX_BUFFER_SIZE) {
     rxWritePos = 0;
+  }
+}
+ISR(USART_TX_vect) {
+  if (txReadPos != txWritePos) {
+    UDR0 = txBuffer[txReadPos++];
+    if (txReadPos >= TX_BUFFER_SIZE) {
+      txReadPos++;
+    }
   }
 }
